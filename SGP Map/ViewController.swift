a20079f9978fdaca24be0686982c428d28bba63a
FAHUU <faighusdeynov87@gmail.com>
2017-03-11 17:47:13 +0400

    Add View controller
    add basic controller

---
+++ SGP Map/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SGP Map
+//
+//  Created by Fahu on 3/11/17.
+//  Copyright © 2017 Fahu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
